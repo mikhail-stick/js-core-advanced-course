89e74e6f572c2d3e020f7e4906f82c0f856dee85
misha-rab-ymniy <mihaildubinka@gmail.com>
2024-06-14 11:21:36 +0300

    add variables-var-let-const module

---
+++ variables-var-let-const/task-1.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 10; i++) {
+    var a = 1;
+    let b = 2;
+    const c = 3;
+}
+
+// После цикла видна будет только перменная a
+console.log(a);
+console.log(b);
+console.log(c);
